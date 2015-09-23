30295a846268726eea98ae10878e0734eefc4eed
Stephan Hagemann <stephan.hagemann@gmail.com>
2015-09-22 23:23:57 -0600

    Workshop: model tests + migration fix

--- ticketee/db/migrate/20150406055658_add_api_key_to_users.rb
+++ ticketee/db/migrate/20150406055658_add_api_key_to_users.rb
@@ -1,4 +1,4 @@
-class AddAPIKeyToUsers < ActiveRecord::Migration
+class AddApiKeyToUsers < ActiveRecord::Migration
   def change
     add_column :users, :api_key, :string
     add_index :users, :api_key
